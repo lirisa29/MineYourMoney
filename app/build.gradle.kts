121a598ead5106f0c1305eef90042c39e752082e
lirisa29 <lirisagovender@gmail.com>
2025-09-27 22:59:37 +0200

    created app logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.iie.thethreeburnouts.mineyourmoney"
-        minSdk = 34
+        minSdk = 32
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
