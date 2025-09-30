d157789ceb266c0c0499c5f419e184158e9aaa85
lirisa29 <lirisagovender@gmail.com>
2025-09-30 09:39:31 +0200

    Created Wallets Screen UI

---
+++ app/src/main/java/com/iie/thethreeburnouts/mineyourmoney/WalletsFragment.kt
@@ -0,0 +1,13 @@
+package com.iie.thethreeburnouts.mineyourmoney
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class WalletsFragment : Fragment(R.layout.fragment_wallets) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set up listeners, adapters, etc.
+    }
+}
