fc3ac6c34b46a90c59e160223c9eabab7b4bf473
lirisa29 <lirisagovender@gmail.com>
2025-11-17 06:11:33 +0200

    Fixed database issue

--- app/src/main/java/com/iie/thethreeburnouts/mineyourmoney/expense/Expense.kt
+++ app/src/main/java/com/iie/thethreeburnouts/mineyourmoney/expense/Expense.kt
@@ -6,12 +6,12 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "expenses") //(Google Developers Training team, 2024)
 data class Expense( //(Google Developers Training team, 2024)
     @PrimaryKey(autoGenerate = true) val id: Int = 0, //(Google Developers Training team, 2024)
-    val userId: Int,
-    val amount: Double,
-    val note: String?,
-    val walletId: Int,
-    val recurrence: String?,
-    val date: Long,
+    val userId: Int = 0,
+    val amount: Double = 0.0,
+    val note: String? = null,
+    val walletId: Int = 0,
+    val recurrence: String? = null,
+    val date: Long = 0L,
     val photoPath: String? = null,
     val updatedAt: Long = System.currentTimeMillis(),
     val deletedAt: Long? = null
